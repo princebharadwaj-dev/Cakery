1b1cfa09e08b637c3ab4949af4cf75ea66862eff
Prince Bharadwaj <princepj951@gmail.com>
2026-08-12 11:28:23 +0530

    Update authController.js

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -34,8 +34,8 @@ export const singUp = async(req, res) => {
         const token = await genToken(user._id)
         res.cookie("token", token, {
             httpOnly:true,
-            secure:false,
-            sameSite:"Strict",
+            secure:true,
+            sameSite:"none",
             maxAge:7 * 24 * 60 * 60 * 1000
         })
         return res.status(201).json(user)
@@ -64,8 +64,8 @@ export const login = async(req, res) => {
         const token = await genToken(user._id)
         res.cookie("token", token, {
             httpOnly:true,
-            secure:false,
-            sameSite:"Strict",
+            secure:true,
+            sameSite:"none",
             maxAge:7 * 24 * 60 * 60 * 100
         })
         return res.status(201).json(user)
@@ -100,8 +100,8 @@ export const googleLogin = async (req,res) => {
         const token = await genToken(user._id)
         res.cookie("token",token,{
         httpOnly:true,
-        secure:false,
-        sameSite: "Strict",
+        secure:true,
+        sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000
     })
     return res.status(200).json(user)
@@ -120,8 +120,8 @@ export const adminLogin = async(req,res) => {
         const token = await genToken1(email)
         res.cookie("token", token, {
             httpOnly:true,
-            secure:false,
-            sameSite:"Strict",
+            secure:true,
+            sameSite:"none",
             maxAge: 1 * 24 * 60 * 60 * 1000
         })
         return res.status(201).json(token)
